e4cc4f549a481f343f837f43db3121e3b62b1058
Neerd <pas25151@gmail.com>
2022-02-16 01:38:21 +0300

    12

--- WebApiTest/Program.cs
+++ WebApiTest/Program.cs
@@ -8,9 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy",
+    options.AddPolicy(MyAllowSpecificOrigins,
         builder => builder
             .AllowAnyOrigin()
             .AllowAnyMethod()
@@ -30,7 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.UseCors();
+app.UseCors(MyAllowSpecificOrigins);
 app.MapControllers();
 
 app.Run();
