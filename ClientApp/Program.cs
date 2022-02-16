6fa3ad785ba69534a19d58d2358d586341350430
Neerd <pas25151@gmail.com>
2022-02-16 10:49:31 +0300

    oh oh oh

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -4,13 +4,6 @@ using ClientApp;
 using Home.Core.Models;
 using Microsoft.Extensions.DependencyInjection;
 
-
-foreach (var a in Enum.GetNames(typeof(EModule)))
-{
-    File.WriteAllText("/home/neerd/RiderProjects/WebApiTest/Home.Core/Modules/" + a + "Module.cs", "");
-}
-
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
